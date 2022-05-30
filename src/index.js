ad7efca946f7e49d50303e490ccd09f036f973c0
siddharth0600 <siddharthtripathi00@gmail.com>
2022-05-30 12:36:59 +0530

    Basic redux explained and implemented

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createStore } from "redux";
+
+//STORE -> Globalized state
+
+//Action Increament
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+//Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+//Dispatch
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
